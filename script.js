0a0a2804cc7ce3fe0bc5bed4c0fddd27c633c2b2
rd1476453 <156095993+rd1476453@users.noreply.github.com>
2025-11-22 09:44:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 //your JS code here. If required.
 function daysOfAYear(year) {
+	
 	if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-		return '366';
+		return 366;
 	}
-	return '365';
+	return 365;
 }
 
 const value = prompt("Enter the Year : ");
 const year = parseInt(value);
 
 
-if( year == null || isNaN(year)){
+if( value === null || isNaN(year)){
 	console.log("Invali Input Year , please enter the valid input !");
 }
 else {
 	const ans = daysOfAYear(year);
 	console.log(ans);
-	alert("The Days in " + year + " : " + ans);
+	alert(ans); 
 }
