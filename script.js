15ec722f7dde66bff60c9964b03370af1b87b762
rd1476453 <156095993+rd1476453@users.noreply.github.com>
2025-11-22 09:32:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your JS code here. If required.
 function daysOfAYear(year) {
 	if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-		return 366;
+		return '366';
 	}
-	return 365;
+	return '365';
 }
 
 const value = prompt("Enter the Year : ");
